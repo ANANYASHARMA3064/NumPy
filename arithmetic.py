ef27db146782c5916a1a8a35e8d022487e4cf88e
Ananya Sharma <ananyasharma3064@gmail.com>
2025-11-18 19:22:18 -0500

    arithmetc in numpy

---
+++ arithmetic.py
@@ -0,0 +1,36 @@
+import numpy as np
+# scalar arithemetic
+# array =np.array([1,2,3]) 
+# print(array+1)
+# print(array*3)
+# print(array**4)
+# vectorized math function 
+# array=np.array([1,2,3])
+# print(np.sqrt(array))
+# print(np.round(array))
+# print(np.floor(array))#round down
+# print(np.ceil(array))#round up
+# print(np.pi)
+# print(np.pi*array**2)
+# element wise arithematic
+# array1=np.array([1,2,3])
+# array2=np.array([4,5,6])
+# print(array1+array2)
+# print(array1**array2)
+#comparision operatoes 
+array =np.array([100,200,90,180])
+# print(array==90) returns a boolean array
+# print(array>90)
+array[array<100]=0
+print(array)
+
+
+
+
+
+
+
+
+
+
+ 
