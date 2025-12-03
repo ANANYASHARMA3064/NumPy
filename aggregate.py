709bb5ea898761bb79b89006b046b93c9cee15b8
Ananya Sharma <ananyasharma3064@gmail.com>
2025-12-02 23:48:21 -0500

    v.busy

--- aggregate.py
+++ aggregate.py
@@ -1,6 +1,6 @@
 import numpy as np
 array = np.array([[1,2,3,4,5],[6,7,8,9,10]])
-print(np.sum(array))
+# print(np.sum(array))
 # print(np.mean(array))
 # print(np.std(array))
 # print(np.var(array))
@@ -11,7 +11,7 @@ print(np.sum(array))
 # print(np.sum(array,axis=1)) sum of rows
  
 
-
+hjh
 
 
 
